121aba80e723ade803ec648efe5b4b4ce7b5eec7
GoKudo0921 <148073393+GoKudo0921@users.noreply.github.com>
2023-12-01 10:49:26 +0900

    Add files via upload

--- NoneApiキー/js/script.js
+++ NoneApiキー/js/script.js
@@ -3,7 +3,7 @@
 $(function () {
   $.ajax({
     type: "GET",
-    url: "https://graph.facebook.com/v6.0/【InstagramビジネスアカウントID】?fields=name%2Cmedia.limit(9)%7Bcaption%2Clike_count%2Cmedia_url%2Cpermalink%2Ctimestamp%2Cthumbnail_url%2Cmedia_type%2Cusername%7D&access_token=【アクセストークン】",
+    url: "https://graph.facebook.com/v6.0/17841400209007101?fields=name%2Cmedia.limit(9)%7Bcaption%2Clike_count%2Cmedia_url%2Cpermalink%2Ctimestamp%2Cthumbnail_url%2Cmedia_type%2Cusername%7D&access_token=EAAMkR43CbbsBOwHcZBorsAqrBihjjrGXy9YqMZBFaytjDPBY1ECSlLwch2rHrn9UmZCSUNzCwGhFd7slxveIRJC1NtY6nBJloZAFQK1r34SDblZAmL7zWzvBXUT4dZCMJTZAWIkzP6IfIwvExmKYDdP0aDkZBLMIedsoZAHWnblEyZBwJFE3Yyxpkzr6PnbiNOZBGMZD",
     dataType: "json",
     success: function (json) {
       var html = "";
